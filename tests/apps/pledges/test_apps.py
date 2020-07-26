7c4ed9be942618548157f12a130009e27fac605b
Mateusz Kieblesz <mattkieblesz@hotmail.com>
2020-07-26 22:47:45 +0200

    Fix test

--- tests/apps/pledges/test_apps.py
+++ tests/apps/pledges/test_apps.py
@@ -4,4 +4,4 @@ from apps.pledges.apps import PledgesConfig
 
 def test_pledges_app():
     assert PledgesConfig.name == 'pledges'
-    assert apps.get_app_config('pledges').name == 'pledges'
+    assert apps.get_app_config('pledges').name == 'apps.pledges'
